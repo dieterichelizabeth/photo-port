fa089c686d8b05a094cca0b8f4276a9cf96a7265
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-04-28 22:21:49 -0500

    Create the Gallery Coponent

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Nav from "./components/Nav";
 import About from "./components/About";
+import Gallery from "./components/Gallery";
 // import logo from "./logo.svg";
 import "./App.css";
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Nav></Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
